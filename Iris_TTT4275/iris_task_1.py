52425a8faa41c1c265a44184d72480e1afcb1551
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-03-20 09:57:58 +0100

    Added some code for calculating mse, not functional yet

--- Iris_TTT4275/iris_task_1.py
+++ Iris_TTT4275/iris_task_1.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Defining constants
+
+N_CLASSES = 3
+N = 50
+N_TRAINING = 30
+
 # Load the data for the Iris Setosa class
 # The data lines are stores in the order: sepal length, sepal width, petal length, petal width - All in cm
 with open("Iris_TTT4275/class_1") as f:
@@ -36,6 +42,7 @@ for flower in virginica_data:
 ### ------------------------------
 ### ------------------------------
 
+'''
 # Plotting the sepal length vs. sepal width for the three classes
 plt.figure(1)
 plt.plot([x[0] for x in setosa], [x[1] for x in setosa], 'ro', label='Setosa')
@@ -57,10 +64,9 @@ plt.title('Petal length vs. petal width')
 plt.legend(['Setosa', 'Versicolor', 'Virginica'])
 
 plt.show()
+'''
 
 ### Task 1a
-N = 50
-N_TRAINING = 30
 
 setosa_training = setosa[:N_TRAINING]
 setosa_testing = setosa[N_TRAINING:]
@@ -71,3 +77,34 @@ versicolor_testing = versicolor[N_TRAINING:]
 virginica_training = virginica[:N_TRAINING]
 virginica_testing = virginica[N_TRAINING:]
 
+### Task 1b
+
+w_matrix = np.ones((N_CLASSES, np.shape(setosa)[1])) # Weights for the three classes and all features
+w0 = 1 # Bias
+# The discriminant vector will be [W w0][x^T 1]^T
+
+T = [[1, 0, 0], 
+     [0, 1, 0], 
+     [0, 0, 1]] # Target vectors
+
+
+alpha = 0.2
+
+#print(np.matmul(w_matrix, setosa_training[0]))
+
+
+
+# Add iterations here when needed
+mse_matrix = np.zeros((N_CLASSES, np.shape(setosa)[1])) # MSE for the three classes and all features
+
+#for sample in setosa_training:
+sample = setosa_training[0]
+i = 0
+
+if i < 30:
+    t = T[0]
+    z = np.matmul(w_matrix, np.transpose(sample))
+    g = 1/(1+np.exp(-z))
+    u = np.multiply((g-t), g, (1-g))
+    print(u)
+    print(sample)
