7f4675d9214a7b3bbecbda16cd154d03cc2f96ed
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-02 23:47:08 +0200

    Did something fucky

--- MNist_ttt4275/ocr_task_1.py
+++ MNist_ttt4275/ocr_task_1.py
@@ -1,3 +1,20 @@
 import numpy as np
 import matplotlib.pyplot
+from PIL import Image
 
+NX = NY = 28
+N = NX*NY
+
+test_samples = []
+
+# Load the test data from the file MNist_ttt4275/test_images.bin which is a binary file
+# with 10000 images of size 28x28
+with open('MNist_ttt4275/test_images.bin', 'rb') as f:
+    for i in range(10000):
+        test_samples.append(np.fromfile(f, dtype=np.uint8, count=28*28))
+
+matrix_samples = []
+
+print_sample = test_samples[0].reshape(NX, NY)
+im = Image.fromarray(print_sample)
+im.show()
