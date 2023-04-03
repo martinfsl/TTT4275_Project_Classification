8c5976373186a29e195bc3d4cf59c9f73a2e9cb4
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-03 11:12:42 +0200

    Added training and testing set

--- MNist_ttt4275/ocr_task_1.py
+++ MNist_ttt4275/ocr_task_1.py
@@ -7,10 +7,12 @@ all_data = sio.loadmat('MNist_ttt4275/data_all.mat')
 #print(all_data.keys())
 #print(all_data.values())
 
-col_size = all_data.get('col_size')[0][0] # Need to add indexes because of the way the data is stored and that we want a scalar
-row_size = all_data.get('row_size')[0][0]
-num_test = all_data.get('num_test')[0][0]
-num_train = all_data.get('num_train')[0][0]
+COL_SIZE = all_data.get('col_size')[0][0] # Need to add indexes because of the way the data is stored and that we want a scalar
+ROW_SIZE = all_data.get('row_size')[0][0]
+NUM_TEST = all_data.get('num_test')[0][0]
+NUM_TRAIN = all_data.get('num_train')[0][0]
+N = all_data.get('vec_size')[0][0]
+NUMBERS = 10 # 0-9
 
 test_labels = all_data.get('testlab')
 test_images = all_data.get('testv')
@@ -20,7 +22,7 @@ train_images = all_data.get('trainv')
 
 #gv = np.reshape(train_images[0], (28, 28))
 
-def plotting(array, rows, cols, title):
+def plotting(array, rows = ROW_SIZE, cols = COL_SIZE, title = ""):
     plot_data = np.reshape(array, (rows, cols))
     plt.gray()
     plt.imshow(plot_data)
@@ -28,3 +30,15 @@ def plotting(array, rows, cols, title):
     plt.show()
 
 #plotting(train_images[6], row_size, col_size, str(train_labels[6][0]))
+
+T = np.identity(NUMBERS) # Target vectors
+
+training = []
+for i in range(NUM_TRAIN):
+    training.append([train_images[i], train_labels[i][0]])
+
+testing = []
+for i in range(NUM_TEST):
+    testing.append([test_images[i], test_labels[i][0]])
+
+
--- OCR.md
+++ OCR.md
@@ -1,6 +1,4 @@
 # This is a file used to brainstorm / draft ideas to solve the ocr task
 
 1. 
-Read the data from the binary files.
-Design a neural network to classify the data.
-Train the network using forward and backward propagation.
+Using nearest neightbour classification.
