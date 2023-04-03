559c6133bef3f190024b9e59c1fad630508c82f4
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-03 09:54:23 +0200

    Added writing of labels

--- MNist_ttt4275/ocr_task_1.py
+++ MNist_ttt4275/ocr_task_1.py
@@ -1,20 +1,29 @@
 import numpy as np
-import matplotlib.pyplot
-from PIL import Image
+import matplotlib.pyplot as plt
 
 NX = NY = 28
 N = NX*NY
 
-test_samples = []
+labels = []
+samples = []
 
 # Load the test data from the file MNist_ttt4275/test_images.bin which is a binary file
 # with 10000 images of size 28x28
-with open('MNist_ttt4275/test_images.bin', 'rb') as f:
-    for i in range(10000):
-        test_samples.append(np.fromfile(f, dtype=np.uint8, count=28*28))
+with open('MNist_ttt4275/train_images.bin', 'rb') as f:
+    for i in range(60000):
+        samples.append(np.fromfile(f, dtype=np.uint8, count=N))
 
-matrix_samples = []
+# Read the labels from the file MNist_ttt4275/test_labels.bin which is a binary file
+with open('MNist_ttt4275/train_labels.bin', 'rb') as f:
+    for i in range(60000):
+        label = np.fromfile(f, dtype=np.uint8, count=1)
+        labels.append(label[0])
 
-print_sample = test_samples[0].reshape(NX, NY)
-im = Image.fromarray(print_sample)
-im.show()
+print(labels[:100])
+print(len(labels))
+
+gv = samples[70].reshape(NX, NY)
+
+plt.gray()
+plt.imshow(gv)
+plt.show()
--- OCR.md
+++ OCR.md
@@ -1,2 +1,6 @@
 # This is a file used to brainstorm / draft ideas to solve the ocr task
 
+1. 
+Read the data from the binary files.
+Design a neural network to classify the data.
+Train the network using forward and backward propagation.
