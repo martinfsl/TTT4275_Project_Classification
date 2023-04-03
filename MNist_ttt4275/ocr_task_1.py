9ba4b2752ec52a4b60e5299b5ecd8ba34175517f
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-03 11:22:03 +0200

    More clean code and some more comments

--- MNist_ttt4275/ocr_task_1.py
+++ MNist_ttt4275/ocr_task_1.py
@@ -2,26 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
+# Dictionary with all the data
 all_data = sio.loadmat('MNist_ttt4275/data_all.mat')
 
-#print(all_data.keys())
-#print(all_data.values())
-
-COL_SIZE = all_data.get('col_size')[0][0] # Need to add indexes because of the way the data is stored and that we want a scalar
-ROW_SIZE = all_data.get('row_size')[0][0]
-NUM_TEST = all_data.get('num_test')[0][0]
-NUM_TRAIN = all_data.get('num_train')[0][0]
+# Defining constants from the data dictionary (all_data)
+# Need to add indexes because of the way the data is stored and that we want a scalar
+COL_SIZE, ROW_SIZE = all_data.get('col_size')[0][0], all_data.get('row_size')[0][0]
+NUM_TEST, NUM_TRAIN = all_data.get('num_test')[0][0], all_data.get('num_train')[0][0]
 N = all_data.get('vec_size')[0][0]
 NUMBERS = 10 # 0-9
 
-test_labels = all_data.get('testlab')
-test_images = all_data.get('testv')
+# Get the data from the dictionary (all_data)
+test_labels, test_images = all_data.get('testlab'), all_data.get('testv')
+train_labels, train_images = all_data.get('trainlab'), all_data.get('trainv')
 
-train_labels = all_data.get('trainlab')
-train_images = all_data.get('trainv')
-
-#gv = np.reshape(train_images[0], (28, 28))
-
+# Function for plotting the images of the digits
 def plotting(array, rows = ROW_SIZE, cols = COL_SIZE, title = ""):
     plot_data = np.reshape(array, (rows, cols))
     plt.gray()
@@ -33,10 +28,10 @@ def plotting(array, rows = ROW_SIZE, cols = COL_SIZE, title = ""):
 
 T = np.identity(NUMBERS) # Target vectors
 
+# Create training and testing data
 training = []
 for i in range(NUM_TRAIN):
     training.append([train_images[i], train_labels[i][0]])
-
 testing = []
 for i in range(NUM_TEST):
     testing.append([test_images[i], test_labels[i][0]])
