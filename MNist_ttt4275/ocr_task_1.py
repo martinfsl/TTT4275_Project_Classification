1701c41e5daf81d1858ae99bc7766d99783e95e6
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-06 18:41:10 +0200

    Added some sketchy, non-functional & super-slow code

--- MNist_ttt4275/ocr_task_1.py
+++ MNist_ttt4275/ocr_task_1.py
@@ -26,8 +26,6 @@ def plotting(array, rows = ROW_SIZE, cols = COL_SIZE, title = ""):
 
 #plotting(train_images[6], row_size, col_size, str(train_labels[6][0]))
 
-T = np.identity(NUMBERS) # Target vectors
-
 # Create training and testing data
 training = []
 for i in range(NUM_TRAIN):
@@ -36,4 +34,35 @@ testing = []
 for i in range(NUM_TEST):
     testing.append([test_images[i], test_labels[i][0]])
 
+wrong = []
+i = 0
 
+for test_sample in testing[760:785]:
+    values = test_sample[0]
+    label = test_sample[1]
+
+    # # Finding the distance between the test sample and all the training samples
+    # distances = []
+    # for train_sample in training:
+    #     distances.append([np.matmul(np.transpose(values - train_sample[0]), values - train_sample[0]), train_sample[1]])
+    
+    # min_index = np.argmin([x[0] for x in distances])
+    # predicted_label = distances[min_index][1]
+
+    # Better code:
+
+    min_distance = np.matmul(np.transpose(values - training[0][0]), values - training[0][0])
+    predicted_label = training[0][1]
+
+    for train_sample in training[1:]:
+        if np.matmul(np.transpose(values - train_sample[0]), values - train_sample[0]) < min_distance:
+            min_distance = np.matmul(np.transpose(values - train_sample[0]), values - train_sample[0])
+            predicted_label = train_sample[1]
+
+    if predicted_label != label:
+        wrong.append([values, label, predicted_label])
+    
+    i += 1
+    print(i)
+
+print("Number of wrong predictions: ", len(wrong))
