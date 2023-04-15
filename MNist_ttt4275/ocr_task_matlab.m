4edc610e1bdae07b9641439580e0bb987ddc6f66
martinfsl <74840025+martinfsl@users.noreply.github.com>
2023-04-15 10:59:40 +0200

    Added MATLAB-figures

--- MNist_ttt4275/ocr_task_matlab.m
+++ MNist_ttt4275/ocr_task_matlab.m
@@ -1,5 +1,5 @@
-disp("Loading data");
-load('data_all.mat')
+% disp("Loading data");
+% load('data_all.mat')
 % disp("Loading pre-saved distances");
 % load('../../Project_Files/distances.mat')
 
@@ -13,44 +13,73 @@ load('data_all.mat')
 % disp("-----------------------");
 % disp("Beginning task 1");
 % 
-% % Task 1 variables
-% 
-% % Used in 1NN-classification
-% % w_1 & c_1 : Number of wrong and correct classifications
-% % cm_1 : Confusion-matrix
-% % wd_1 & wl_1 : Array containing data and labels respectively for wrongly classified images
-% % cd_1 & cl_1 : Same as the above, only with correctly classified images
-% % tp_lab_1 : An array that holds the true and predicted labels, used for plotting
-% % The label matrices contain [True label, Predicted label]
-% [w_1, c_1, cm_1, wd_1, wl_1, cd_1, cl_1, tp_lab_1] = deal(0, 0, zeros(10, 10), zeros(1, vec_size), zeros(1, 2), zeros(1, vec_size), zeros(1, 2), zeros(10000, 2));
-% 
-% % Same as the above, only that they are used in the 7NN-classification
-% [w_1_7, c_1_7, cm_1_7, wd_1_7, wl_1_7, cd_1_7, cl_1_7, tp_lab_1_7] = deal(0, 0, zeros(10, 10), zeros(1, vec_size), zeros(1, 2), zeros(1, vec_size), zeros(1, 2), zeros(10000, 2));
-% 
-% size_bulk = 999; % 1000 - 1 (needs to be removed)
-% for i = 1:(num_test/size_bulk)
-%     % Splitting the testing-sets into 'bulks' of 1000 elements, calculating
-%     % distances for each of these sets. Adding all togheter to a new matrix
-% %     distances_sets = zeros(num_train, size_bulk+1); 
-%     distances_set = distances(:, i:i+size_bulk);
-%     testing_data = testv(i:i+size_bulk, :);
-%     testing_labels = testlab(i:i+size_bulk, :);
-%     [number_of_tests, ~] = size(testing_data);
-%     
-% %     disp("Calculating distances");
-% %     distances_sets = calculate_distance(testing_data, trainv);
-%     disp("Classifying");
-%     % 1-NN classifier:
-%     [cm_1, wd_1, wl_1, w_1, cd_1, cl_1, c_1, tp_lab_1] = classify_1NN(distances_set, number_of_tests, testing_data, testing_labels, trainlab, cm_1, wd_1, wl_1, w_1, cd_1, cl_1, c_1, tp_lab_1);
-%     % 7-NN classifier:
-%     [cm_1_7, wd_1_7, wl_1_7, w_1_7, cd_1_7, cl_1_7, c_1_7, tp_lab_1_7] = classify_kNN(distances_set, number_of_tests, testing_data, testing_labels, trainlab, cm_1_7, wd_1_7, wl_1_7, w_1_7, cd_1_7, cl_1_7, c_1_7, 7, tp_lab_1_7);
-% end
+% % % Task 1 variables
+% % 
+% % % Used in 1NN-classification
+% % % w_1 & c_1 : Number of wrong and correct classifications
+% % % cm_1 : Confusion-matrix
+% % % wd_1 & wl_1 : Array containing data and labels respectively for wrongly classified images
+% % % cd_1 & cl_1 : Same as the above, only with correctly classified images
+% % % tp_lab_1 : An array that holds the true and predicted labels, used for plotting
+% % % The label matrices contain [True label, Predicted label]
+% % [w_1, c_1, cm_1, wd_1, wl_1, cd_1, cl_1, tp_lab_1] = deal(0, 0, zeros(10, 10), zeros(1, vec_size), zeros(1, 2), zeros(1, vec_size), zeros(1, 2), zeros(0, 2));
+% % 
+% % % Same as the above, only that they are used in the 7NN-classification
+% % [w_1_7, c_1_7, cm_1_7, wd_1_7, wl_1_7, cd_1_7, cl_1_7, tp_lab_1_7] = deal(0, 0, zeros(10, 10), zeros(1, vec_size), zeros(1, 2), zeros(1, vec_size), zeros(1, 2), zeros(0, 2));
+% % 
+% % disp('Calculating distances and classifying');
+% % size_bulk = 999; % 1000 - 1 (needs to be removed)
+% % for i = 1:(num_test/size_bulk)
+% %     % Splitting the testing-sets into 'bulks' of 1000 elements, calculating
+% %     % distances for each of these sets. Adding all togheter to a new matrix
+% %     distances_set = distances(:, i:i+size_bulk);
+% %     testing_data = testv(i:i+size_bulk, :);
+% %     testing_labels = testlab(i:i+size_bulk, :);
+% %     [number_of_tests, ~] = size(testing_data);
+% %     
+% % %     % Calculating distances for the bulk of testing data
+% % %     distances_set = calculate_distance(testing_data, trainv);
+% %     
+% %     % 1-NN classifier:
+% %     [cm_1, wd_1, wl_1, w_1, cd_1, cl_1, c_1, tp_lab_1] = classify_1NN(distances_set, number_of_tests, testing_data, testing_labels, trainlab, cm_1, wd_1, wl_1, w_1, cd_1, cl_1, c_1, tp_lab_1);
+% %     % 7-NN classifier:
+% %     [cm_1_7, wd_1_7, wl_1_7, w_1_7, cd_1_7, cl_1_7, c_1_7, tp_lab_1_7] = classify_kNN(distances_set, number_of_tests, testing_data, testing_labels, trainlab, cm_1_7, wd_1_7, wl_1_7, w_1_7, cd_1_7, cl_1_7, c_1_7, 7, tp_lab_1_7);
+% % end
 % 
 % error_rate_1 = w_1/num_test;
+% disp("Error-rate for the 1NN-classifier for the unclustered data: " + error_rate_1);
 % error_rate_1_7 = w_1_7/num_test;
+% disp("Error-rate for the 7NN-classifier for the unclustered data: " + error_rate_1_7);
 % 
-% % Plot one wrongly classified and one correctly classified
-% % plotting(w_data, w_labels, num_w, c_data, c_labels, num_c, col_size, row_size);
+% % Plotting the confusion matrices
+% plot_confusion_matrix(tp_lab_1, "Confusion matrix for the unclustered data using the 1NN-classifier");
+% pause(5);
+% plot_confusion_matrix(tp_lab_1_7, "Confusion matrix for the unclustered data using the 7NN-classifier");
+% pause(5);
+% 
+% % 1NN
+% % Picking three random correctly classified digits to plot
+% [ri_1_c1, ri_1_c2, ri_1_c3] = deal(randi(length(cl_1), 1), randi(length(cl_1), 1), randi(length(cl_1), 1));
+% labels_c_1 = [cl_1(ri_1_c1, :); cl_1(ri_1_c2, :); cl_1(ri_1_c3, :)];
+% images_c_1 = [cd_1(ri_1_c1, :); cd_1(ri_1_c2, :); cd_1(ri_1_c3, :)];
+% plotting_3_images(images_c_1, labels_c_1, col_size, row_size, 'Three randomly selected correctly classified digits for the 1NN-classifier using unclustered templates');
+% % Picking three random wrongly classified digits to plot
+% [ri_1_w1, ri_1_w2, ri_1_w3] = deal(randi(length(wl_1), 1), randi(length(wl_1), 1), randi(length(wl_1), 1));
+% labels_w_1 = [wl_1(ri_1_w1, :); wl_1(ri_1_w2, :); wl_1(ri_1_w3, :)];
+% images_w_1 = [wd_1(ri_1_w1, :); wd_1(ri_1_w2, :); wd_1(ri_1_w3, :)];
+% plotting_3_images(images_w_1, labels_w_1, col_size, row_size, 'Three randomly selected wrongly classified digits for the 1NN-classifier using unclustered templates');
+% 
+% % 7NN
+% % Picking three random correctly classified digits to plot
+% [ri_1_c1_7, ri_1_c2_7, ri_1_c3_7] = deal(randi(length(cl_1_7), 1), randi(length(cl_1_7), 1), randi(length(cl_1_7), 1));
+% labels_c_1_7 = [cl_1_7(ri_1_c1_7, :); cl_1_7(ri_1_c2_7, :); cl_1_7(ri_1_c3_7, :)];
+% images_c_1_7 = [cd_1_7(ri_1_c1_7, :); cd_1_7(ri_1_c2_7, :); cd_1_7(ri_1_c3_7, :)];
+% plotting_3_images(images_c_1_7, labels_c_1_7, col_size, row_size, 'Three randomly selected correctly classified digits for the 7NN-classifier using unclustered templates');
+% % Picking three random wrongly classified digits to plot
+% [ri_1_w1_7, ri_1_w2_7, ri_1_w3_7] = deal(randi(length(wl_1_7), 1), randi(length(wl_1_7), 1), randi(length(wl_1_7), 1));
+% labels_w_1_7 = [wl_1_7(ri_1_w1_7, :); wl_1_7(ri_1_w2_7, :); wl_1_7(ri_1_w3_7, :)];
+% images_w_1_7 = [wd_1_7(ri_1_w1_7, :); wd_1_7(ri_1_w2_7, :); wd_1_7(ri_1_w3_7, :)];
+% plotting_3_images(images_w_1_7, labels_w_1_7, col_size, row_size, 'Three randomly selected wrongly classified digits for the 7NN-classifier using unclustered templates');
 % 
 % disp("Ending task 1");
 % disp("-----------------------");
@@ -67,22 +96,22 @@ load('data_all.mat')
 %%%------------------------------------
 %%%------------------------------------
 
-% disp("-----------------------");
-% disp("Beginning task 2");
-%  
-% % disp("Sorting & Clustering");
-% % 
-% % % Returns a 1x10 cell
-% % % Each element, f. ex. trainv_sorted{1} contains all the data with label 0 (digit 0)
-% % trainv_sorted = sorting(trainv, trainlab, num_train, vec_size);
-% % 
-% % M = 64;
-% % new_training_set = zeros(10*M, vec_size);
-% % 
-% % for i = 0:9
-% %    [~, Ci] = kmeans(trainv_sorted{i+1}, M);
-% %    new_training_set(i*M+1:(i+1)*M, :) = Ci;
-% % end
+disp("-----------------------");
+disp("Beginning task 2");
+ 
+% disp("Sorting & Clustering");
+% 
+% % Returns a 1x10 cell
+% % Each element, f. ex. trainv_sorted{1} contains all the data with label 0 (digit 0)
+% trainv_sorted = sorting(trainv, trainlab, num_train, vec_size);
+% 
+% M = 64;
+% new_training_set = zeros(10*M, vec_size);
+% 
+% for i = 0:9
+%    [~, Ci] = kmeans(trainv_sorted{i+1}, M);
+%    new_training_set(i*M+1:(i+1)*M, :) = Ci;
+% end
 % 
 % % Task 2 variables
 % 
@@ -98,6 +127,7 @@ load('data_all.mat')
 % % 7-NN classifier:
 % [w_2_7, c_2_7, cm_2_7, wd_2_7, wl_2_7, cd_2_7, cl_2_7, tp_lab_2_7] = deal(0, 0, zeros(10, 10), zeros(1, vec_size), zeros(1, 2), zeros(1, vec_size), zeros(1, 2), zeros(0, 2));
 % 
+% disp("Calculating distances and classifying");
 % size_bulk = 999; % 1000 - 1 (needs to be removed)
 % for i = 1:(num_test/(size_bulk+1))
 %     % Splitting the testing-sets into 'bulks' of 1000 elements, calculating
@@ -108,9 +138,7 @@ load('data_all.mat')
 %     
 %     training_labels = [0*ones(M, 1); 1*ones(M,1); 2*ones(M,1); 3*ones(M,1); 4*ones(M,1); 5*ones(M,1); 6*ones(M,1); 7*ones(M,1); 8*ones(M,1); 9*ones(M,1)];
 %     
-%     disp("Calculating distances");
 %     distances_clustering = calculate_distance(testing_data, new_training_set);
-%     disp("Classifying");
 %     
 %     % 1-NN classifier
 %     [cm_2, wd_2, wl_2, w_2, cd_2, cl_2, c_2, tp_lab_2] = classify_1NN(distances_clustering, number_of_tests, testing_data, testing_labels, training_labels, cm_2, wd_2, wl_2, w_2, cd_2, cl_2, c_2, tp_lab_2);
@@ -119,23 +147,48 @@ load('data_all.mat')
 % end
 % 
 % error_rate_2 = w_2/num_test;
+% disp("Error-rate for the 1NN-classifier for the clustered data: " + error_rate_2);
 % error_rate_2_7 = w_2_7/num_test;
-% 
-% disp("Ending task 2");
-% disp("-----------------------");
+% disp("Error-rate for the 7NN-classifier for the clustered data: " + error_rate_2_7);
+
+% % Plotting the confusion matrices
+% plot_confusion_matrix(tp_lab_2, "Confusion matrix for the clustered data using the 1NN-classifier");
+% pause(5);
+% plot_confusion_matrix(tp_lab_2_7, "Confusion matrix for the clustered data using the 7NN-classifier");
+% pause(5);
+
+% 1NN
+% Picking three random correctly classified digits to plot
+[ri_2_c1, ri_2_c2, ri_2_c3] = deal(randi(length(cl_2), 1), randi(length(cl_2), 1), randi(length(cl_2), 1));
+labels_c_2 = [cl_2(ri_2_c1, :); cl_2(ri_2_c2, :); cl_2(ri_2_c3, :)];
+images_c_2 = [cd_2(ri_2_c1, :); cd_2(ri_2_c2, :); cd_2(ri_2_c3, :)];
+plotting_3_images(images_c_2, labels_c_2, col_size, row_size, 'Three randomly selected correctly classified digits for the 1NN-classifier using clustered templates');
+% Picking three random wrongly classified digits to plot
+[ri_2_w1, ri_2_w2, ri_2_w3] = deal(randi(length(wl_2), 1), randi(length(wl_2), 1), randi(length(wl_2), 1));
+labels_w_2 = [wl_2(ri_2_w1, :); wl_2(ri_2_w2, :); wl_2(ri_2_w3, :)];
+images_w_2 = [wd_2(ri_2_w1, :); wd_2(ri_2_w2, :); wd_2(ri_2_w3, :)];
+plotting_3_images(images_w_2, labels_w_2, col_size, row_size, 'Three randomly selected wrongly classified digits for the 1NN-classifier using clustered templates');
+
+% 7NN
+% Picking three random correctly classified digits to plot
+[ri_2_c1_7, ri_2_c2_7, ri_2_c3_7] = deal(randi(length(cl_2_7), 1), randi(length(cl_2_7), 1), randi(length(cl_2_7), 1));
+labels_c_2_7 = [cl_2_7(ri_2_c1_7, :); cl_2_7(ri_2_c2_7, :); cl_2_7(ri_2_c3_7, :)];
+images_c_2_7 = [cd_2_7(ri_2_c1_7, :); cd_2_7(ri_2_c2_7, :); cd_2_7(ri_2_c3_7, :)];
+plotting_3_images(images_c_2_7, labels_c_2_7, col_size, row_size, 'Three randomly selected correctly classified digits for the 7NN-classifier using clustered templates');
+% Picking three random wrongly classified digits to plot
+[ri_2_w1_7, ri_2_w2_7, ri_2_w3_7] = deal(randi(length(wl_2_7), 1), randi(length(wl_2_7), 1), randi(length(wl_2_7), 1));
+labels_w_2_7 = [wl_2_7(ri_2_w1_7, :); wl_2_7(ri_2_w2_7, :); wl_2_7(ri_2_w3_7, :)];
+images_w_2_7 = [wd_2_7(ri_2_w1_7, :); wd_2_7(ri_2_w2_7, :); wd_2_7(ri_2_w3_7, :)];
+plotting_3_images(images_w_2_7, labels_w_2_7, col_size, row_size, 'Three randomly selected wrongly classified digits for the 7NN-classifier using clustered templates');
+
+disp("Ending task 2");
+disp("-----------------------");
 
 %%%------------------------------------
 %%%------------------------------------
 %%%         END OF TASK 2
 %%%------------------------------------
 %%%------------------------------------
-
-% plotting(wd_2(1, :), col_size, row_size, wl_2(1, 1), wl_2(1, 2));
-
-% cm_plot = confusionchart(true_pred_lab_2(:, 1), true_pred_lab_2(:, 2));
-plot_confusion_matrix(tp_lab_2, "");
-pause(10);
-plot_confusion_matrix(tp_lab_2_7, "");
 
 % Classifying the test-vectors given the distance matrix using 1NN.
 % Returns confusion-matrix, data, labels and amount for wrong and correct classification. 
@@ -196,9 +249,25 @@ function [cm, wd, wl, w, cd, cl, c, true_pred_lab] = classify_1NN(distances_set,
 end
 
 % Plotting randomly picked wrongly classified and correctly classified
-function plotting(image_data, col_size, row_size, tl, pl)
+function plotting_images(image_data, labels, col_size, row_size)
     image(transpose(reshape(image_data, col_size, row_size)));
-    title("True label: " + num2str(tl) + " - Classified label: " + num2str(pl));
+    title("True label - " + labels(1, 1) + " / Predicted label - " + labels(1, 2));
+end
+
+function plotting_3_images(image_datas, labels, col_size, row_size, title_overall)
+    figure('Position', [100 100 800 300]);
+    subplot('Position', [0.05 0.1 0.25 0.8]);
+    imshow(transpose(reshape(image_datas(1, :), col_size, row_size)), []);
+    title("True label - " + labels(1, 1) + " / Predicted label - " + labels(1, 2));
+    subplot('Position', [0.35 0.1 0.25 0.8]);
+    imshow(transpose(reshape(image_datas(2, :), col_size, row_size)), []);
+    title("True label - " + labels(2, 1) + " / Predicted label - " + labels(2, 2));
+    subplot('Position', [0.65 0.1 0.25 0.8]);
+    imshow(transpose(reshape(image_datas(3, :), col_size, row_size)), []);
+    title("True label - " + labels(3, 1) + " / Predicted label - " + labels(3, 2));
+
+    % Overall title
+    sgtitle(title_overall);
 end
 
 % Calculates the distances given test-vector and templates
@@ -210,10 +279,10 @@ function s = sorting(training_data, training_label, num_train, vec_size)
     s = {zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size), zeros(0, vec_size)};
     for j = 1:num_train
         s{training_label(j)+1}(end+1, :) = training_data(j, :);
-%         disp(j);
     end
 end
 
-function plot_confusion_matrix(true_pred_labels, title)
+function plot_confusion_matrix(true_pred_labels, title_cm)
     confusionchart(true_pred_labels(:, 1), true_pred_labels(:, 2));
+    title(title_cm);
 end
